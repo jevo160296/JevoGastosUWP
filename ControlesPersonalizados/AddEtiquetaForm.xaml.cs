fabada49f6aab15819f743bf9b3a7eb340f56793
Jose Miguel Millan Londoño <jevo160296@gmail.com>
2020-06-06 00:28:51 -0500

    V Alpha 0.0.2 * Se agregó un control personalizado para añadir etiquetas. * Se agregaron diccionarios de recursos para personalizar el diseño de la app dependiendo del tema actual de windows y sus colores de acento. * Mejoras en el diseño de la app. * Se habilitaron controles para eliminar etiquetas y transacciones.

---
+++ ControlesPersonalizados/AddEtiquetaForm.xaml.cs
@@ -0,0 +1,100 @@
+﻿using JevoGastosCore;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Control de usuario está documentada en https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace JevoGastosUWP.ControlesPersonalizados
+{
+    public sealed partial class AddEtiquetaForm : UserControl
+    {
+        public static readonly DependencyProperty TipoEtiquetaProperty =
+            DependencyProperty.Register(
+                "TipoEtiqueta",
+                typeof(TipoEtiqueta),
+                typeof(UserControl),
+                new PropertyMetadata(default(TipoEtiqueta)));
+        public static readonly DependencyProperty ContainerProperty =
+            DependencyProperty.Register(
+                "Container",
+                typeof(GastosContainer),
+                typeof(UserControl),
+                new PropertyMetadata(default(GastosContainer)));
+        public static readonly DependencyProperty HeaderBackgroundProperty =
+            DependencyProperty.Register(
+                "HeaderBackground",
+                typeof(Brush),
+                typeof(UserControl),
+                new PropertyMetadata(new SolidColorBrush(Windows.UI.Color.FromArgb(1,1,1,1)))
+                );
+        public static readonly DependencyProperty HeaderForegroundProperty =
+            DependencyProperty.Register(
+                "HeaderForeground",
+                typeof(Brush),
+                typeof(UserControl),
+                new PropertyMetadata(new SolidColorBrush(Windows.UI.Color.FromArgb(1, 0, 0, 0)))
+                );
+        public static readonly DependencyProperty HeaderTextProperty =
+            DependencyProperty.Register(
+                "HeaderText",
+                typeof(string),
+                typeof(UserControl),
+                new PropertyMetadata("Nueva etiqueta")
+                );
+
+        public TipoEtiqueta TipoEtiqueta
+        {
+            get => (TipoEtiqueta)GetValue(TipoEtiquetaProperty);
+            set => SetValue(TipoEtiquetaProperty, value);
+        }
+        public GastosContainer Container
+        {
+            get => (GastosContainer)GetValue(ContainerProperty);
+            set => SetValue(ContainerProperty, value);
+        }
+        public Brush HeaderBackground
+        {
+            get => (Brush)GetValue(HeaderBackgroundProperty);
+            set => SetValue(HeaderBackgroundProperty, value);
+        }
+        public Brush HeaderForeground
+        {
+            get => (Brush)GetValue(HeaderForegroundProperty);
+            set => SetValue(HeaderForegroundProperty, value);
+        }
+        public string HeaderText
+        {
+            get => (string)GetValue(HeaderTextProperty);
+            set => SetValue(HeaderTextProperty, value);
+        }
+        public TextBox TextBox
+        {
+            get => TB_Name;
+        }
+
+        public AddEtiquetaForm()
+        {
+            this.InitializeComponent();
+        }
+
+        private void B_Save_Click(object sender, RoutedEventArgs e)
+        {
+            Click?.Invoke(this, e);
+        }
+
+        public event RoutedEventHandler Click;
+        
+    }
+}
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,11 +1,13 @@
 ﻿using JevoGastosCore;
 using JevoGastosCore.Model;
 using JevoGastosCore.ModelView;
+using JevoGastosUWP.ControlesPersonalizados;
 using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Navigation;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
@@ -51,7 +53,9 @@ namespace JevoGastosUWP
         private ObservableCollection<Transaccion> Transacciones => Container.TransaccionDAO.Items;
         private ObservableCollection<Transaccion> RelatedTrans => relatedTrans;
         private VisibilityHandler ErrorVisibility = new VisibilityHandler();
+        private VisibilityHandler TransValorFormatError = new VisibilityHandler();
         private VisibilityHandler TipoSelected = new VisibilityHandler();
+        private bool EditandoTrans = false;
 
         public MainPage()
         {
@@ -65,7 +69,23 @@ namespace JevoGastosUWP
         }
         private void ConfigureView()
         {
+            TipoSelected.PropertyChanged-= TipoSelected_PropertyChanged;
+            TipoSelected.PropertyChanged += TipoSelected_PropertyChanged;
         }
+
+        private void TipoSelected_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            if ((sender as VisibilityHandler).Visibility==Visibility.Collapsed)
+            {
+                TransValorFormatError.Visibility = Visibility.Collapsed;
+                SP_TransForm.Spacing = 0;
+            }
+            else
+            {
+                SP_TransForm.Spacing = 8;
+            }
+        }
+
         private void AddIngreso(string name)
         {
             Container.IngresoDAO.Add(name);
@@ -205,19 +225,29 @@ namespace JevoGastosUWP
 
         private void AddIngreso_Click(object sender, RoutedEventArgs e)
         {
-            AddIngreso(TB_NuevoIngreso.Text);
+            AddIngreso(((AddEtiquetaForm)sender).TextBox.Text);
         }
         private void AddCuenta_Click(object sender, RoutedEventArgs e)
         {
-            AddCuenta(TB_NuevaCuenta.Text);
+            AddCuenta(((AddEtiquetaForm)sender).TextBox.Text);
         }
         private void AddGasto_Click(object sender, RoutedEventArgs e)
         {
-            AddGasto(TB_NuevoGasto.Text);
+            AddGasto(((AddEtiquetaForm)sender).TextBox.Text);
         }
         private void AddTransaccion_Click(object sender, RoutedEventArgs e)
         {
-            AddTransaccion(CB_Origen.SelectedItem as Etiqueta, CB_Destino.SelectedItem as Etiqueta, double.Parse(TB_Valor.Text), TB_Descripcion.Text, CDP_Fecha.Date.Value.Date);
+            double valor;
+            EditandoTrans = false;
+            if (!double.TryParse(TB_Valor.Text, out valor))
+            {
+                valor = 0;
+            }
+            Etiqueta origen = CB_Origen.SelectedItem as Etiqueta;
+            Etiqueta destino = CB_Destino.SelectedItem as Etiqueta;
+            string descripcion = TB_Descripcion.Text;
+            DateTime? date = CDP_Fecha.Date is null ? null : (DateTime?)CDP_Fecha.Date.Value.Date;
+            AddTransaccion(origen, destino, valor, descripcion, date);
         }
 
         private void DIngreso_Click(object sender, RoutedEventArgs e)
@@ -236,7 +266,14 @@ namespace JevoGastosUWP
         {
             DelTransaccion();
         }
-
+        private void ResetTransaccionForm()
+        {
+            CDP_Fecha.Date = DateTime.Now;
+            CB_Origen.SelectedIndex = -1;
+            CB_Destino.SelectedIndex = -1;
+            TB_Valor.Text = "";
+            TB_Descripcion.Text = "";
+        }
         private void CB_TipoSelected(object sender, SelectionChangedEventArgs e)
         {
             ComboBox cb = (ComboBox)sender;
@@ -247,6 +284,12 @@ namespace JevoGastosUWP
             }
             else
             {
+                if (TipoSelected.Visibility==Visibility.Collapsed)
+                {
+                    ResetTransaccionForm();
+                    TransFormDataValidation();
+                }
+                EditandoTrans = true;
                 TipoSelected.Visibility = Visibility.Visible;
                 switch (index)
                 {
@@ -254,21 +297,162 @@ namespace JevoGastosUWP
                     case 0:
                         CB_Origen.ItemsSource = Ingresos;
                         CB_Destino.ItemsSource = Cuentas;
+                        CB_Origen.PlaceholderText = "Ingresos";
+                        CB_Destino.PlaceholderText = "Cuentas";
                         break;
                     //Movimiento
                     case 1:
                         CB_Origen.ItemsSource = Cuentas;
                         CB_Destino.ItemsSource = Cuentas;
+                        CB_Origen.PlaceholderText = "Cuentas";
+                        CB_Destino.PlaceholderText = "Cuentas";
                         break;
                     //Salida
                     case 2:
                         CB_Origen.ItemsSource = Cuentas;
                         CB_Destino.ItemsSource = Gastos;
+                        CB_Origen.PlaceholderText = "Cuentas";
+                        CB_Destino.PlaceholderText = "Gastos";
                         break;
                     default:
                         break;
                 }
             }
         }
+
+        private void LV_ContainerContentChanging(ListViewBase sender, ContainerContentChangingEventArgs args)
+        {
+            var deleteCommand = new StandardUICommand(StandardUICommandKind.None);
+            var editCommand = new StandardUICommand(StandardUICommandKind.None);
+            Etiqueta data = args.Item as Etiqueta;
+            MenuFlyout menuFlyout = new MenuFlyout();
+            MenuFlyoutItem eliminar = new MenuFlyoutItem() 
+            { 
+                Text = "Eliminar",
+                Icon=new SymbolIcon(Symbol.Delete),
+                Command=deleteCommand,
+                CommandParameter=data
+            };
+            MenuFlyoutItem editar = new MenuFlyoutItem() 
+            {
+                Text = "Editar", 
+                Icon = new SymbolIcon(Symbol.Edit) ,
+                Command=editCommand,
+                CommandParameter=data
+            };
+            eliminar.Click += Eliminar_Click;
+            editar.Click += Editar_Click;
+            menuFlyout.Items.Add(eliminar);
+            menuFlyout.Items.Add(editar);
+            args.ItemContainer.ContextFlyout = menuFlyout;
+        }
+        private void LV_Transacciones_ContainerContentChanging(ListViewBase sender, ContainerContentChangingEventArgs args)
+        {
+            var deleteCommand = new StandardUICommand(StandardUICommandKind.None);
+            var editCommand = new StandardUICommand(StandardUICommandKind.None);
+            Transaccion data = args.Item as Transaccion;
+            MenuFlyout menuFlyout = new MenuFlyout();
+            MenuFlyoutItem eliminar = new MenuFlyoutItem()
+            {
+                Text = "Eliminar",
+                Icon = new SymbolIcon(Symbol.Delete),
+                Command = deleteCommand,
+                CommandParameter = data
+            };
+            MenuFlyoutItem editar = new MenuFlyoutItem()
+            {
+                Text = "Editar",
+                Icon = new SymbolIcon(Symbol.Edit),
+                Command = editCommand,
+                CommandParameter = data
+            };
+            eliminar.Click += EliminarTrans_Click;
+            editar.Click += EditarTrans_Click;
+            menuFlyout.Items.Add(eliminar);
+            menuFlyout.Items.Add(editar);
+            args.ItemContainer.ContextFlyout = menuFlyout;
+        }
+
+        private void Editar_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        private void EditarTrans_Click(object sender, RoutedEventArgs e) { }
+
+        private async void Eliminar_Click(object sender, RoutedEventArgs e)
+        {
+            Etiqueta etiqueta = (Etiqueta)(((MenuFlyoutItem)sender).CommandParameter);
+            CD_Eliminar.Content = $"¿Está seguro que desea eliminar {etiqueta.Name}?";
+            CD_Eliminar.Visibility = Visibility.Visible;
+            ContentDialogResult contentDialogResult = await CD_Eliminar.ShowAsync();
+            switch (contentDialogResult)
+            {
+                case ContentDialogResult.Primary:
+                    try
+                    {
+                        EtiquetaDAO.Delete(etiqueta, Container);
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                    break;
+            }
+        }
+        private void EliminarTrans_Click(object sender, RoutedEventArgs e)
+        {
+            Transaccion transaccion = (Transaccion)(((MenuFlyoutItem)sender).CommandParameter);
+            Container.TransaccionDAO.Remove(transaccion);
+        }
+
+        private void AddTransFlyout_Closing(Windows.UI.Xaml.Controls.Primitives.FlyoutBase sender, Windows.UI.Xaml.Controls.Primitives.FlyoutBaseClosingEventArgs args)
+        {
+            if (!EditandoTrans)
+            {
+                CB_Tipo.SelectedIndex = -1;
+            }
+        }
+
+        private bool TransFormDataValidation()
+        {
+            bool correcto;
+            correcto = 
+                !(CDP_Fecha.Date is null) & 
+                CB_Origen.SelectedIndex != -1 &
+                CB_Destino.SelectedIndex != -1 &
+                (TB_Valor.Text is null ? false : (double.TryParse(TB_Valor.Text, out double _)));
+            APB_GuardarTrans.IsEnabled = correcto;
+            return correcto;
+        }
+
+        private void CDP_Fecha_DateChanged(CalendarDatePicker sender, CalendarDatePickerDateChangedEventArgs args)
+        {
+            TransFormDataValidation();
+        }
+
+        private void CB_Origen_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            TransFormDataValidation();
+        }
+
+        private void TB_Valor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (!TransFormDataValidation() & !double.TryParse(TB_Valor.Text, out double _))
+            {
+                TransValorFormatError.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                TransValorFormatError.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void AppBarCancelTransButton_Click(object sender, RoutedEventArgs e)
+        {
+            EditandoTrans = false;
+            var flyout = ABB_AddTrans.Flyout;
+            flyout.Hide();
+        }
+
     }
 }
